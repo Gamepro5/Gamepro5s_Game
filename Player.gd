fb218ce3690659d747b756082e36aa534c72f3dc
Gamepro5 <gamepro543@gmail.com>
2020-06-03 10:24:05 -0700

    Very Shitty Gravity Implementation
    Jumping not included

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 
 var velocity = Vector3(0,0,0);
+var upDef = Vector3(0, 1, 0)
 const accelerationConstant = 0.5;
 var maxSpeed = 0.04; 
 var acceleration;
@@ -16,14 +17,17 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		if cursorFocus:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 			rotate(Vector3.UP, deg2rad(-event.relative.x * sensitivity));
 			eyes.rotate(Vector3.RIGHT, deg2rad(-event.relative.y * sensitivity));
 			if eyes.get_rotation().x > PI/2:
 				eyes.set_rotation(Vector3(PI/2, 0, 0))
 			elif eyes.get_rotation().x < -PI/2:
 				eyes.set_rotation(Vector3(-PI/2, 0, 0))
-		
+	
+	if Input.is_action_pressed("unlock_cursor"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		cursorFocus = false;	
 func _physics_process(delta):
 	
 	if velocity.x != 0 || velocity.z != 0:
@@ -35,12 +39,17 @@ func _physics_process(delta):
 			velocity.x = 0;
 		if abs(velocity.z) <= 0.0001:
 			velocity.z = 0;
-#	
-#	if midAir:
-#		acceleration = accelerationConstant * 0.15;
-#	else:
-#		acceleration = accelerationConstant;
-		
+			
+	
+	if !is_on_floor():
+		#set_translation(Vector3(get_translation().x, 0, get_translation().z))
+		acceleration = accelerationConstant * 0.15;
+		velocity.y -= 0.1;
+	else:
+		velocity.y = 0;
+		acceleration = accelerationConstant;
+	print(is_on_floor())
+	
 	if Input.is_action_pressed("move_front"):
 		#velocity.z = velocity.z - 1;
 		velocity.x -= sin(rotation.y) #* acceleration;
@@ -61,11 +70,19 @@ func _physics_process(delta):
 		rotate(Vector3.UP, -0.1)
 	if Input.is_action_pressed("turn_left"):
 		rotate(Vector3.UP, 0.1)
-	move_and_slide(velocity)
+	move_and_slide(velocity, upDef) #allways be moving
 	
 	if Input.is_action_pressed("unlock_cursor"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		cursorFocus = false;
-	#print(velocity)
-
-	pass
+		
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_WM_FOCUS_IN:
+		get_viewport().warp_mouse(Vector2(get_viewport().size.x/2,get_viewport().size.y/2))
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		cursorFocus = true;
+		print("focus in")
+	elif what == MainLoop.NOTIFICATION_WM_FOCUS_OUT:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		print("focus out")
