1e4b1d4e89441327c2f31f933ea60234c8d2460b
Gamepro5 <gamepro543@gmail.com>
2020-06-04 11:58:38 -0700

    Mouse input, jumping, and better physics.

--- Player.gd
+++ Player.gd
@@ -2,13 +2,15 @@ extends KinematicBody
 
 var velocity = Vector3(0,0,0);
 var upDef = Vector3(0, 1, 0)
-const accelerationConstant = 0.5;
-var maxSpeed = 0.04; 
-var acceleration;
+const accelerationConstant = 0.8;
+var maxSpeed = 50; 
+var acceleration = 0;
 var midAir = false;
 var cursorFocus = true;
 onready var eyes = get_node("Camera");
 var sensitivity = 0.3;
+var gravity = 25;
+var jumpStrength = 10;
 
 func _ready():
 	
@@ -29,6 +31,7 @@ func _input(event):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		cursorFocus = false;	
 func _physics_process(delta):
+	midAir = !is_on_floor();
 	
 	if velocity.x != 0 || velocity.z != 0:
 		if !midAir:
@@ -41,41 +44,53 @@ func _physics_process(delta):
 			velocity.z = 0;
 			
 	
-	if !is_on_floor():
+	if midAir:
+		velocity.y = velocity.y - (gravity * delta);
+		
 		#set_translation(Vector3(get_translation().x, 0, get_translation().z))
-		acceleration = accelerationConstant * 0.15;
-		velocity.y -= 0.1;
+		acceleration = accelerationConstant / 5;
 	else:
 		velocity.y = 0;
 		acceleration = accelerationConstant;
-	print(is_on_floor())
 	
+	var velocityVector = Vector3(velocity.x, 0, velocity.z);
+	#if the hypotenuse of the right triangle (overall velocity) is too long
+	if (velocityVector.length() > maxSpeed):
+		#shrink the triangle so that its hypotenuse is the max speed
+		velocityVector = velocityVector.normalized();
+		velocityVector.x *= maxSpeed;
+		velocityVector.z *= maxSpeed;
+		#set the velocities to the new legs of the triangle
+		velocity.x = velocityVector.x;
+		velocity.z = velocityVector.z;
+		
 	if Input.is_action_pressed("move_front"):
 		#velocity.z = velocity.z - 1;
-		velocity.x -= sin(rotation.y) #* acceleration;
-		velocity.z -= cos(rotation.y) #* acceleration;
+		velocity.x -= sin(rotation.y) * acceleration;
+		velocity.z -= cos(rotation.y) * acceleration;
 	if Input.is_action_pressed("move_left"):
 		#velocity.x = velocity.x - 1;
-		velocity.x -= cos(rotation.y) #* acceleration;
-		velocity.z += sin(rotation.y) #* acceleration;
+		velocity.x -= cos(rotation.y) * acceleration;
+		velocity.z += sin(rotation.y) * acceleration;
 	if Input.is_action_pressed("move_back"):
 		#velocity.x = velocity.x + 1;
-		velocity.x += sin(rotation.y) #* acceleration;
-		velocity.z += cos(rotation.y) #* acceleration;
+		velocity.x += sin(rotation.y) * acceleration;
+		velocity.z += cos(rotation.y) * acceleration;
 	if Input.is_action_pressed("move_right"):
 		#velocity.z = velocity.z + 1;
-		velocity.x += cos(rotation.y) #* acceleration;
-		velocity.z -= sin(rotation.y) #* acceleration;
-	if Input.is_action_pressed("turn_right"):
-		rotate(Vector3.UP, -0.1)
-	if Input.is_action_pressed("turn_left"):
-		rotate(Vector3.UP, 0.1)
+		velocity.x += cos(rotation.y) * acceleration;
+		velocity.z -= sin(rotation.y) * acceleration;
+	if Input.is_action_pressed("jump"):
+		if (!midAir):
+			velocity.y = jumpStrength;
 	move_and_slide(velocity, upDef) #allways be moving
 	
 	if Input.is_action_pressed("unlock_cursor"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		cursorFocus = false;
-		
+	
+	print("tick = ", delta)
+	
 func _notification(what):
 	if what == MainLoop.NOTIFICATION_WM_FOCUS_IN:
 		get_viewport().warp_mouse(Vector2(get_viewport().size.x/2,get_viewport().size.y/2))
@@ -86,3 +101,4 @@ func _notification(what):
 	elif what == MainLoop.NOTIFICATION_WM_FOCUS_OUT:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		print("focus out")
+		
