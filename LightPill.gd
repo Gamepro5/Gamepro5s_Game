e05eca3ba6416ec00163d72f8625333f6a7fe325
Gamepro5 <gamepro543@gmail.com>
2020-06-10 13:43:59 -0700

    progress twoards a weapon system

--- LightPill.gd
+++ LightPill.gd
@@ -1,9 +1,1 @@
 extends RigidBody
-
-#var velocity = Vector3(0,0,0)
-
-	
-#func _integrate_forces( state ):
-#		print(2)
-#		add_force(Vector3(-43,70,0), translation)
-#		#state.linear_velocity = Vector3(-43,70,0)
--- PlayerData/Code/Player.gd
+++ PlayerData/Code/Player.gd
@@ -86,21 +86,21 @@ func _physics_process(delta):
 		velocity.z = velocityVector.z;
 		
 	if Input.is_action_pressed("move_front"):
-		#velocity.z = velocity.z - 1;
-		velocity.x -= sin(head.global_transform.basis.get_euler().y) * acceleration;
-		velocity.z -= cos(head.global_transform.basis.get_euler().y) * acceleration;
+		#velocity.z = velocity.z - 1;head.global_transform.basis.get_euler().y
+		velocity.x -= sin(rotation.y) * acceleration;
+		velocity.z -= cos(rotation.y) * acceleration;
 	if Input.is_action_pressed("move_left"):
 		#velocity.x = velocity.x - 1;
-		velocity.x -= cos(head.global_transform.basis.get_euler().y) * acceleration;
-		velocity.z += sin(head.global_transform.basis.get_euler().y) * acceleration;
+		velocity.x -= cos(rotation.y) * acceleration;
+		velocity.z += sin(rotation.y) * acceleration;
 	if Input.is_action_pressed("move_back"):
 		#velocity.x = velocity.x + 1;
-		velocity.x += sin(head.global_transform.basis.get_euler().y) * acceleration;
-		velocity.z += cos(head.global_transform.basis.get_euler().y) * acceleration;
+		velocity.x += sin(rotation.y) * acceleration;
+		velocity.z += cos(rotation.y) * acceleration;
 	if Input.is_action_pressed("move_right"):
 		#velocity.z = velocity.z + 1;
-		velocity.x += cos(head.global_transform.basis.get_euler().y) * acceleration;
-		velocity.z -= sin(head.global_transform.basis.get_euler().y) * acceleration;
+		velocity.x += cos(rotation.y) * acceleration;
+		velocity.z -= sin(rotation.y) * acceleration;
 	if Input.is_action_pressed("jump"):
 		if (!midAir):
 			velocity.y = jumpStrength;
