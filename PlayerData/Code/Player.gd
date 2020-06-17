1d62b0287c1b82b54588e817921e441b82ba8219
Gamepro5 <gamepro543@gmail.com>
2020-06-17 08:11:31 -0700

    more terrible art

--- PlayerData/Code/Player.gd
+++ PlayerData/Code/Player.gd
@@ -72,31 +72,31 @@ func _physics_process(delta):
 		#set the velocities to the new legs of the triangle
 		velocity.x = velocityVector.x;
 		velocity.z = velocityVector.z;
-		
-	if Input.is_action_pressed("move_front"):
-		#velocity.z = velocity.z - 1;head.global_transform.basis.get_euler().y
-		velocity.x -= sin(rotation.y) * acceleration;
-		velocity.z -= cos(rotation.y) * acceleration;
-	if Input.is_action_pressed("move_left"):
-		#velocity.x = velocity.x - 1;
-		velocity.x -= cos(rotation.y) * acceleration;
-		velocity.z += sin(rotation.y) * acceleration;
-	if Input.is_action_pressed("move_back"):
-		#velocity.x = velocity.x + 1;
-		velocity.x += sin(rotation.y) * acceleration;
-		velocity.z += cos(rotation.y) * acceleration;
-	if Input.is_action_pressed("move_right"):
-		#velocity.z = velocity.z + 1;
-		velocity.x += cos(rotation.y) * acceleration;
-		velocity.z -= sin(rotation.y) * acceleration;
-	if Input.is_action_pressed("jump"):
-		if (!midAir):
-			velocity.y = jumpStrength;
-	if Input.is_action_just_pressed("action_1"):
-		#get_viewport().warp_mouse(Vector2(get_viewport().size.x/2,get_viewport().size.y/2))
-		if (head.get_node("Weapon").get_child_count() != 0):
-			emit_signal("shoot", self)
-			#print(head.translation)
+	if (cursorFocus):
+		if Input.is_action_pressed("move_front"):
+			#velocity.z = velocity.z - 1;head.global_transform.basis.get_euler().y
+			velocity.x -= sin(rotation.y) * acceleration;
+			velocity.z -= cos(rotation.y) * acceleration;
+		if Input.is_action_pressed("move_left"):
+			#velocity.x = velocity.x - 1;
+			velocity.x -= cos(rotation.y) * acceleration;
+			velocity.z += sin(rotation.y) * acceleration;
+		if Input.is_action_pressed("move_back"):
+			#velocity.x = velocity.x + 1;
+			velocity.x += sin(rotation.y) * acceleration;
+			velocity.z += cos(rotation.y) * acceleration;
+		if Input.is_action_pressed("move_right"):
+			#velocity.z = velocity.z + 1;
+			velocity.x += cos(rotation.y) * acceleration;
+			velocity.z -= sin(rotation.y) * acceleration;
+		if Input.is_action_pressed("jump"):
+			if (!midAir):
+				velocity.y = jumpStrength;
+		if Input.is_action_just_pressed("action_1"):
+			#get_viewport().warp_mouse(Vector2(get_viewport().size.x/2,get_viewport().size.y/2))
+			if (head.get_node("Weapon").get_child_count() != 0):
+				emit_signal("shoot", self)
+				#print(head.translation)
 	move_and_slide(velocity, upDef) #allways be moving
 	
 
