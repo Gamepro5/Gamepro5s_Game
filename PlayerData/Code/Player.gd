156df3d94957eda1c17e12e8016fb6af230c14f2
Gamepro5 <gamepro543@gmail.com>
2020-07-02 19:22:09 -0700

    slowly dying inside

--- PlayerData/Code/Player.gd
+++ PlayerData/Code/Player.gd
@@ -7,16 +7,15 @@ var maxSpeed = 5;
 var acceleration = 0;
 var midAir = false;
 var cursorFocus = false;
-var sensitivity = 0.3;
+var mouse_sensitivity = 0.3; #0.3
 var gravity = 25;
 var jumpStrength = 10;
-var flashlightOn = true;
-var unshaded_material;
-var shaded_material;
 var type = "player";
 var health = 100;
 onready var head = get_node("Head");
 onready var eyes = head.get_node("Eyes");
+
+var _snap: Vector3
 
 signal shoot(weilder);
 
@@ -25,11 +24,22 @@ func kill():
 	queue_free();
 
 func _ready():
-
+	
 	connect("shoot", head.get_node("Weapon").get_node("Gun"), "on_player_shoot") #gun
 	pass
-	
+
+
 func _input(event):
+	if event is InputEventMouseMotion:
+		if cursorFocus:
+			rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+			head.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
+
+			# Clamp mouse rotation
+			var temp_rot: Vector3 = head.rotation_degrees
+			temp_rot.x = clamp(temp_rot.x, -90, 90)
+			head.rotation_degrees = temp_rot
+	"""
 	if event is InputEventMouseMotion:
 		if cursorFocus:
 			rotate(Vector3.UP, deg2rad(-event.relative.x * sensitivity));
@@ -40,13 +50,15 @@ func _input(event):
 			elif head.get_rotation().x < -PI/2:
 				head.set_rotation(Vector3(-PI/2, 0, 0))
 				pass
-	
+	"""
 	if Input.is_action_pressed("unlock_cursor"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		cursorFocus = false;	
+		
+		
 func _physics_process(delta):
 	midAir = !is_on_floor();
-	
+
 	if velocity.x != 0 || velocity.z != 0:
 		if !midAir:
 			velocity.z *= 0.9;
@@ -57,13 +69,22 @@ func _physics_process(delta):
 		if abs(velocity.z) <= 0.0001:
 			velocity.z = 0;
 			
-	velocity.y = velocity.y - (gravity * delta);
+	#Gravity is a constant force, you know.
+	velocity.y -= gravity * delta;
+	
 	if midAir:
 		#velocity.y = velocity.y - (gravity * delta);
 		acceleration = accelerationConstant / 5;
 	else:
-		velocity.y = -1;
+		velocity.y = -(gravity * delta);
+		#velocity.y = velocity.y - (gravity * delta);
+		#print(velocity.y)
 		acceleration = accelerationConstant;
+	#print(Vector3(velocity.x, 0, velocity.z).length())
+	if midAir:
+		get_tree().get_root().get_node("Game/UI/Label").set_text("Midair? True")
+	else:
+		get_tree().get_root().get_node("Game/UI/Label").set_text("Midair? False")
 	
 	var velocityVector = Vector3(velocity.x, 0, velocity.z);
 	#if the hypotenuse of the right triangle (overall velocity) is too long
@@ -92,16 +113,24 @@ func _physics_process(delta):
 			#velocity.z = velocity.z + 1;
 			velocity.x += cos(rotation.y) * acceleration;
 			velocity.z -= sin(rotation.y) * acceleration;
-		if Input.is_action_just_pressed("jump"):
-			if (!midAir):
-				velocity.y = jumpStrength;
+			
+		if !midAir:
+			_snap = Vector3(0, -1, 0)
+			if Input.is_action_just_pressed("jump"):
+				_snap = Vector3(0, 0, 0)
+				velocity.y = jumpStrength
 		if Input.is_action_just_pressed("action_1"):
 			#get_viewport().warp_mouse(Vector2(get_viewport().size.x/2,get_viewport().size.y/2))
 			if (head.get_node("Weapon").get_child_count() != 0):
 				emit_signal("shoot", self)
 				#print(head.translation)
-	move_and_slide(velocity, upDef) #allways be moving
+	#move_and_slide(velocity, upDef)
+	if (midAir):
+		move_and_slide(velocity, upDef, true, 4, deg2rad(45)) #allways be moving
+	else:
+		move_and_slide_with_snap(velocity, _snap, Vector3(0, 1, 0), true, 4, deg2rad(45))
 	
+	#print(_snap)
 
 	if Input.is_action_pressed("lock_cursor"):
 		#get_viewport().warp_mouse(Vector2(get_viewport().size.x/2,get_viewport().size.y/2))
@@ -111,7 +140,6 @@ func _physics_process(delta):
 	if Input.is_action_pressed("unlock_cursor"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		cursorFocus = false;
-	
 	#print("tick = ", delta)
 func _notification(what):
 	if what == MainLoop.NOTIFICATION_WM_FOCUS_IN:
